e21ba4853f60809dab09beba94bbea99c75d9276
reneebetina <betinaesperas@gmail.com>
2022-07-26 15:58:10 +0800

    Create replace_file_extension.py

---
+++ replace_file_extension.py
@@ -0,0 +1,10 @@
+filenames = ["program.c", "stdio.hpp", "sample.hpp", "a.out", "math.hpp", "hpp.out"]
+# Generate newfilenames as a list containing the new filenames
+# using as many lines of code as your chosen method requires.
+newfilenames = [ x.replace('.hpp','.h') for x in filenames if '.hpp' in x or '.'in x]
+
+
+print(newfilenames) 
+# Should be ["program.c", "stdio.h", "sample.h", "a.out", "math.h", "hpp.out"]
+
+
