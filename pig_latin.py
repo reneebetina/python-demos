265ce3a412738ae34dc068d804ea4109f0940028
reneebetina <betinaesperas@gmail.com>
2022-07-26 15:59:03 +0800

    Create pig_latin.py

---
+++ pig_latin.py
@@ -0,0 +1,13 @@
+def pig_latin(text):
+  say = ""
+  # Separate the text into words
+  words = text.split()
+  for word in words:
+    # Create the pig latin word and add it to the list
+    x= word[1:len(word)]+word[0]+'ay'
+    say = say + x + " "
+    # Turn the list back into a phrase
+  return say.strip()
+		
+print(pig_latin("hello how are you")) # Should be "ellohay owhay reaay ouyay"
+print(pig_latin("programming in python is fun")) # Should be "rogrammingpay niay ythonpay siay unfay"
