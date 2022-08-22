fd6e49afda307b2f037900c2fadbf36ae17f1b70
reneebetina <betinaesperas@gmail.com>
2022-08-22 21:26:16 +0800

    show sorted values

--- dictionary_samples/sort_by_key_using_list_of_dict.py
+++ dictionary_samples/sort_by_key_using_list_of_dict.py
@@ -18,3 +18,33 @@ print(sorted(people, key=lambda i: i['name']))
 # by both age and name.
 print("\nSort by AGE then NAME: ")
 print(sorted(people, key=lambda i: (i['age'], i['name'])))
+
+
+
+#
+#Sort by AGE ONLY: 
+#[{'name': 'Annie', 'age': 16}, 
+#{'name': 'Zoe', 'age': 18}, 
+#{'name': 'Jill', 'age': 19}, 
+#{'name': 'Jane', 'age': 20}, 
+#{'name': 'Barbie', 'age': 20}, 
+#{'name': 'Jack', 'age': 20}, 
+#{'name': 'Abby', 'age': 23}]
+
+#Sort by NAME ONLY: 
+#[{'name': 'Abby', 'age': 23}, 
+#{'name': 'Annie', 'age': 16},
+#{'name': 'Barbie', 'age': 20}, 
+#{'name': 'Jack', 'age': 20}, 
+#{'name': 'Jane', 'age': 20}, 
+#{'name': 'Jill', 'age': 19}, 
+#{'name': 'Zoe', 'age': 18}]
+
+#Sort by AGE then NAME: 
+#[{'name': 'Annie', 'age': 16}, 
+#{'name': 'Zoe', 'age': 18}, 
+#{'name': 'Jill', 'age': 19}, 
+#{'name': 'Barbie', 'age': 20}, 
+#{'name': 'Jack', 'age': 20}, 
+#{'name': 'Jane', 'age': 20}, 
+#{'name': 'Abby', 'age': 23}]
