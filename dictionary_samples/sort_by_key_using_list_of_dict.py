4d39b3af86a63271734fc6aaf9f7b5f0dd25eab8
Renee Betina Esperas <betinaesperas@gmail.com>
2022-08-22 21:21:27 +0800

    sorting example

---
+++ dictionary_samples/sort_by_key_using_list_of_dict.py
@@ -0,0 +1,20 @@
+people = [{"name": "Jane", "age": 20},
+          {"name": "Barbie", "age": 20},
+          {"name": "Jill", "age": 19},
+          {"name": "Zoe", "age": 18},
+          {"name": "Annie", "age": 16},
+          {"name": "Jack", "age": 20},
+          {"name": "Abby", "age": 23},
+          ]
+
+# by age
+print("Sort by AGE ONLY: ")
+print(sorted(people, key=lambda i: i['age']))
+
+# by age
+print("\nSort by NAME ONLY: ")
+print(sorted(people, key=lambda i: i['name']))
+
+# by both age and name.
+print("\nSort by AGE then NAME: ")
+print(sorted(people, key=lambda i: (i['age'], i['name'])))
